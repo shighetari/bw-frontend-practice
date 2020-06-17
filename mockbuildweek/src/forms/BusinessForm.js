578db7df6e23db5e2422bdba2230f8b1bc364865
Buddy Agyin <agyin3@gmail.com>
2020-06-16 23:47:52 -0600

    Create user auth forms

---
+++ mockbuildweek/src/forms/BusinessForm.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import axios from 'axios'
+import { useLocation } from 'react-router-dom'
+
+const BusinessForm = () => {
+    // const { pathname } = useLocation()
+    const [pathname, setPathname] = useState('/registe') // remove once connected to routes
+    const { register, handleSubmit, errors, reset } = useForm();
+    
+    const handleRegister = data => {
+        if(data.password === data.password_verify) {
+            delete data.password_verify
+            axios.post('https://food-bw-practice-backend.herokuapp.com/api/auth/bussiness-owner/register', data)
+                .then(res => {
+                    console.log(res.data)
+                    reset()
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        }
+    };
+
+    const handleLogin = data => {
+        axios.post('https://food-bw-practice-backend.herokuapp.com/api/auth/bussiness-owner/login', data)
+        .then(res => {
+            console.log(res.data)
+            reset()
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+    
+    
+    return (
+        <>
+      <form onSubmit={pathname.includes('/register') ? handleSubmit(handleRegister) : handleSubmit(handleLogin)}>
+        <input type="text" placeholder="Username" name="username" ref={register({required: true, maxLength: 80})} />
+        <input type="password" placeholder="Password" name="password" ref={register({required: true, minLength: 5})} />
+        {
+        pathname.includes('register') &&
+        <>
+            <input type="password" placeholder="Re-enter Password" name="password_verify" ref={register({required: true, minLength: 5})} />
+            <input type="text" placeholder="Email" name="email" ref={register({required: true, pattern: /^\S+@\S+$/i})} />
+            <input type="text" placeholder="First name" name="first_name" ref={register({required: false, maxLength: 80})} />
+            <input type="text" placeholder="Last name" name="last_name" ref={register({required: false, maxLength: 100})} />
+
+            <input type="text" placeholder="Business Name" name="business_name" ref={register({required: true, maxLength: 80})} />
+        </>
+        }
+        <input type="submit" />
+      </form>
+      </>
+    )
+}
+
+export default BusinessForm
